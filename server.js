6dfa292d552ce7b273840ee9886071a947a04ece
Andre-Junior <andrejunior.dias@gmail.com>
2020-10-02 20:54:42 -0300

    Update signin

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const { response } = require('express');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1', //localhost
-      user : 'postgres',
-      password : '123',
-      database : 'smart_brain'
+        host: '127.0.0.1', //localhost
+        user: 'postgres',
+        password: '123',
+        database: 'smart_brain'
     }
 });
 
@@ -20,66 +20,63 @@ const app = express();
 app.use(express.json());
 app.use(cors())
 
-const database = {
-    users: [
-        {
-            id: '123',
-            name: 'John',
-            password: 'fruta',
-            email: 'john.1@gmail.com',
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id: '124',
-            name: 'Sally',
-            password: 'verduras',
-            email: 'sally.sally@gmail.com',
-            entries: 0,
-            joined: new Date()
-        }
-    ],
-    login: [
-        {
-            id: '987',
-            hash: '',
-            email: 'john.1@gmail.com'
-        }
-    ]
-}
 
 app.get('/', (req, res) => {
     res.send(database.users)
 })
 
 app.post('/signin', (req, res) => {
-    if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
-        res.json(database.users[0]);
-    } else {
-        res.status(400).json('Login incorreto')
-    }
+    db.select('email', 'hash').from('login')
+    .where('email', '=', req.body.email)
+    .then(data => {
+        const isValid = bcrypt.compareSync(req.body.password, data[0].hash)
+        if (isValid) {
+            return db.select('*').from('users')
+                .where('email', '=', req.body.email)
+                .then(user => {
+                    res.json(user[0])
+                })
+                .catch(err => res.status(400).json('Não foi possivel fazer login! Email ou senha incorretos!'))
+        } else {
+            res.status(400).json('Email ou senha incorretos!')
+        }
+    })
+    .catch(err => res.status(400).json('Email ou senha incorretos!'))
 })
 
-app.post('/register', (req,res) => {
-    const { email, name, password} = req.body
-   db('users')
-   .returning('*')   
-   .insert({
-       email: email,
-       name: name,
-       joined: new Date()
-   })
-    .then(user => {
-        res.json(user[0])
+app.post('/register', (req, res) => {
+    const { email, name, password } = req.body
+    const hash = bcrypt.hashSync(password)
+    db.transaction(trx => {
+        trx.insert({
+            hash: hash,
+            email: email
+        })
+            .into('login')
+            .returning('email')
+            .then(loginEmail => {
+                return trx('users')
+                    .returning('*')
+                    .insert({
+                        email: loginEmail[0],
+                        name: name,
+                        joined: new Date()
+                    })
+                    .then(user => {
+                        res.json(user[0])
+                    })
+            })
+            .then(trx.commit)
+            .catch(trx.rollback)
     })
-    .catch(err => res.status(400).json('Não foi possivel o registro!'))
+        .catch(err => res.status(400).json('Não foi possivel o registro!'))
 })
 
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
-    db.select('*').from('users').where({id})
+    db.select('*').from('users').where({ id })
         .then(user => {
-            if(user.length){
+            if (user.length) {
                 res.json(user[0])
             } else {
                 res.status(400).json('Usuário não encontrado')
